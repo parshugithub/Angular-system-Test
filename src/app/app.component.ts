b92c3297dfc8094098084f260b5520a2b8673185
parshuram <maddelaparsharamulu@gmail.com>
2021-06-23 19:15:09 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './model/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'interviewAngular';
+  user = new User();
+
+  register(){
+
+  }
 }
---
+++ src/app/model/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    FirstName:string;
+    LastName:string;
+    EmailId:string;
+    ContactNumber:string;
+    Password:string;
+    ConfirmPassword:string; 
+}
